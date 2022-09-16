89c578ab2540f994bcff713664b72cf8de1af584
souzajr <oi@robertosouza.dev>
2022-09-16 11:16:11 -0300

    update with express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import express from 'express';
 import startCronJob from './cronJob';
 
 startCronJob();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  // eslint-disable-next-line no-console
+  console.log('\x1b[41m\x1b[37m', `Server running on port ${port}`, '\x1b[0m')
+);
